e659c3123c6e80bcf6f3b26e1a97e37f8c0feefb
Taylor Collins <taylorcc33@gmail.com>
2020-11-18 23:14:15 -0700

    added second model

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,10 +1,12 @@
 class DepartmentsController < ApplicationController
+
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
+
   def index
     @department = Department.all
   end
 
   def show
-    @department = Department.find(params[:id])
   end
 
   def new
@@ -22,17 +24,21 @@ class DepartmentsController < ApplicationController
   end
 
   def edit
-    @department = Department.find(params[:id])
     render partial: "form"
   end
 
   def update
-    @department = Department.find(params[:id])
     if @department.update(department_params)
-      redirect_to @department 
+      redirect_to department_path(@department)
     else
       render :edit
     end
+  end
+
+  def destroy
+    @department.destroy
+   
+    redirect_to departments_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  # get 'items/index'
+  # get 'items/show'
+  # get 'items/new'
+  # get 'items/edit'
   root "departments#index"
   # get 'departments/index'
   # get 'departments/show'
   # get 'departments/new'
   # get 'departments/edit'
-  resources :departments
+  resources :departments do
+    resources :items
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
